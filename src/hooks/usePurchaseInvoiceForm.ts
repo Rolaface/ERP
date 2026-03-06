abe91df0ec1a095b6589645ad98a67e2a4382006
Shivangi-03 <shivangi.sharma@rolaface.com>
2026-03-06 15:40:13 +0530

    feat:fix

--- src/hooks/usePurchaseInvoiceForm.ts
+++ src/hooks/usePurchaseInvoiceForm.ts
@@ -268,7 +268,7 @@ export const usePurchaseInvoiceForm = ({
         taxCategory: data.taxCategory || "",
         project: data.project || "",
         costCenter: data.costCenter || "",
-        shippingRule: data.shippingRule || "",
+        // shippingRule: data.shippingRule || "",
         incoterm:
           typeof data.incoterm === "string"
             ? data.incoterm.trim().toUpperCase()
