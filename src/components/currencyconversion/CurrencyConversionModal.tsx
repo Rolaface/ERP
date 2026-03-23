d6b07f742db195484f99a83c6d30a0a73a104752
rohit15rawat <rohitrawat0212@gmail.com>
2026-03-23 12:01:25 +0530

    feat: ui change

--- src/components/currencyconversion/CurrencyConversionModal.tsx
+++ src/components/currencyconversion/CurrencyConversionModal.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { FaExchangeAlt } from "react-icons/fa";
+import {FaCalendarAlt} from "react-icons/fa";
 import Modal from "../../components/ui/modal/modal";
 import { Button } from "../../components/ui/modal/formComponent";
 import { ModalInput } from "../../components/ui/modal/modalComponent";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import DatePickerInput from "../calendar/DatePickerInput";
+
 
 interface Props {
   isOpen: boolean;
@@ -37,7 +37,6 @@ const CurrencyConversionModal: React.FC<Props> = ({
       alert("Please fill required fields");
       return;
     }
-
     onSave?.(form);
     onClose();
   };
@@ -60,71 +59,76 @@ const CurrencyConversionModal: React.FC<Props> = ({
       title="Currency Conversion"
       subtitle="Add exchange rate"
       footer={footer}
-      customWidth="50vw"
-      height="auto"
+      customWidth="fit-content"
+      height="70vh"
     >
-      <div className="p-6">
+      <div className="p-4">
 
         {/* FORM */}
-        <div className="flex gap-4 items-end overflow-x-auto">
+        <div className="flex flex-wrap gap-3 items-end">
 
-  {/* DATE */}
-  <div className="min-w-[180px]">
-    <label className="text-sm text-gray-600">Date</label>
-    <DatePicker
-      selected={form.date}
-      onChange={(date: Date | null) =>
-        setForm((prev) => ({ ...prev, date }))
-      }
-      dateFormat="dd/MM/yyyy"
-      placeholderText="Select date"
-      className="w-full border border-[var(--border)] rounded-lg px-3 py-2 mt-1"
-    />
-  </div>
+          {/* DATE */}
+          <div className="flex-1 min-w-[100px] max-w-[140px] relative">
+  <label className="text-xs text-gray-600">Date</label>
 
-  {/* FROM */}
-  <div className="min-w-[160px]">
-    <ModalInput
-      label="From Currency"
-      name="fromCurrency"
-      value={form.fromCurrency}
-      onChange={handleChange}
-    />
-  </div>
+  <DatePickerInput
+    selected={form.date}
+    onChange={(date: Date | null) =>
+      setForm((prev) => ({ ...prev, date }))
+    }
+    dateFormat="dd/MM/yyyy"
+    placeholderText="Select date"
+    className="w-full border border-[var(--border)] rounded-md px-2 pr-8 py-1 mt-1 text-xs"
+    
+  />
 
-  {/* TO */}
-  <div className="min-w-[160px]">
-    <ModalInput
-      label="To Currency"
-      name="toCurrency"
-      value={form.toCurrency}
-      onChange={handleChange}
-    />
-  </div>
-
-  {/* BUY */}
-  <div className="min-w-[140px]">
-    <ModalInput
-      label="Buy Rate"
-      name="buyRate"
-      type="number"
-      value={form.buyRate}
-      onChange={handleChange}
-    />
-  </div>
-
-  {/* SELL */}
-  <div className="min-w-[140px]">
-    <ModalInput
-      label="Sell Rate"
-      name="sellRate"
-      type="number"
-      value={form.sellRate}
-      onChange={handleChange}
-    />
-  </div>
-
+  {/* ICON */}
+  <FaCalendarAlt className="absolute right-2 top-[28px] text-gray-400 text-sm pointer-events-none" />
 </div>
+
+          {/* FROM */}
+          <div className="flex-1 min-w-[100px] max-w-[140px]">
+            <ModalInput
+              label="From Currency"
+              name="fromCurrency"
+              value={form.fromCurrency}
+              onChange={handleChange}
+            />
+          </div>
+
+          {/* TO */}
+          <div className="flex-1 min-w-[100px] max-w-[140px]">
+            <ModalInput
+              label="To Currency"
+              name="toCurrency"
+              value={form.toCurrency}
+              onChange={handleChange}
+            />
+          </div>
+
+          {/* BUY */}
+          <div className="flex-1 min-w-[100px] max-w-[140px]">
+            <ModalInput
+              label="Buy Rate"
+              name="buyRate"
+              type="number"
+              value={form.buyRate}
+              onChange={handleChange}
+            />
+          </div>
+
+          {/* SELL */}
+          <div className="flex-1 min-w-[100px] max-w-[140px]">
+            <ModalInput
+              label="Sell Rate"
+              name="sellRate"
+              type="number"
+              value={form.sellRate}
+              onChange={handleChange}
+            />
+          </div>
+
+        </div>
       </div>
     </Modal>
   );
