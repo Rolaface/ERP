3b112d4cd6b5e581d8483ea5c466e424a892a21e
Akhilesh Rawat <akhileshrawat.asr@gmail.com>
2026-03-26 16:32:55 +0530

    fix: deployment for real this time

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [tailwindcss(), react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_BASE_URL,
-          changeOrigin: true,
-          secure: false,
-          cookieDomainRewrite: "localhost",
-          configure: (proxy) => {
-            proxy.on("proxyRes", (proxyRes) => {
-              const cookies = proxyRes.headers["set-cookie"];
-              if (cookies) {
-                proxyRes.headers["set-cookie"] = cookies.map((cookie) =>
-                  cookie
-                    .replace(/;\s*Secure/gi, "")
-                    .replace(/;\s*SameSite=None/gi, "; SameSite=Lax"),
-                );
-              }
-            });
-          },
-        },
-      },
-    },
-  };
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
 });
