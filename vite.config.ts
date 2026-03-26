a7a19882a1dc9b602c1c0884a35c6a09c0110f1f
Akhilesh Rawat <akhileshrawat.asr@gmail.com>
2026-03-26 15:58:24 +0530

    deployment(proxy): use env VITE_API_BASE_URL  to proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
- 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [tailwindcss(), react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          cookieDomainRewrite: "localhost",
+          configure: (proxy) => {
+            proxy.on("proxyRes", (proxyRes) => {
+              const cookies = proxyRes.headers["set-cookie"];
+              if (cookies) {
+                proxyRes.headers["set-cookie"] = cookies.map((cookie) =>
+                  cookie
+                    .replace(/;\s*Secure/gi, "")
+                    .replace(/;\s*SameSite=None/gi, "; SameSite=Lax"),
+                );
+              }
+            });
+          },
+        },
+      },
+    },
+  };
 });
