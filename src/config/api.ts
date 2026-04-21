7a27548e360fff97fdb866cd0fc8a443a3070e10
Manoj Chand <memanojkumain@gmail.com>
2026-04-21 14:52:07 +0530

    Update ERP_BASE constant to use an empty string

--- src/config/api.ts
+++ src/config/api.ts
@@ -49,7 +49,7 @@ const getApiBaseUrl = (): string => {
 // console.log("🚀 ~ ERP_BASE:", ERP_BASE)
 
 // export const ERP_BASE = ENV.apiBaseUrl;
- export const ERP_BASE = "";
+export const ERP_BASE = "";
 export const CODES_BASE = ENV.zraCodesBaseUrl;
 export const NAPSA_BASE = ENV.napsaBaseUrl;
 
