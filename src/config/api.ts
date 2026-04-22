eda218847537eeff64071ff616c6732397dcf498
Akhilesh Rawat <akhileshrawat.asr@gmail.com>
2026-04-22 13:33:26 +0530

    .

--- src/config/api.ts
+++ src/config/api.ts
@@ -30,7 +30,7 @@ const getApiBaseUrl = (): string => {
   return `${protocol}//api.erp.${tenantSubdomain}.${baseDomain}`;
 };
 
-export const ERP_BASE = ENV.apiBaseUrl;
+export const ERP_BASE = getApiBaseUrl();
 //  export const ERP_BASE = "";
 export const CODES_BASE = ENV.zraCodesBaseUrl;
 export const NAPSA_BASE = ENV.napsaBaseUrl;
