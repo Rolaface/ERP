b4a3a9bad9751aaaa960331997c7d4a58736cec7
rohit15rawat <rohitrawat0212@gmail.com>
2026-05-07 13:58:31 +0530

    invoice

--- src/components/template/invoice/invoiceTemplatRolaface.tsx
+++ src/components/template/invoice/invoiceTemplatRolaface.tsx
@@ -109,7 +109,7 @@ doc.setFontSize(8);
   if (compEmail) { doc.text(compEmail, M, curY); curY += 4; }
 
   const logoRightX = MR;
-  const logoTopY = 21;
+  const logoTopY = 12;
 
   if (company?.documents?.companyLogoUrl) {
     try {
