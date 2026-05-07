aff17a6a52b463db7fe8c911839765bd82bfee99
rohit15rawat <rohitrawat0212@gmail.com>
2026-05-07 13:53:42 +0530

    changes

--- src/components/template/invoice/invoiceTemplatRolaface.tsx
+++ src/components/template/invoice/invoiceTemplatRolaface.tsx
@@ -109,7 +109,7 @@ doc.setFontSize(8);
   if (compEmail) { doc.text(compEmail, M, curY); curY += 4; }
 
   const logoRightX = MR;
-  const logoTopY = 12;
+  const logoTopY = 21;
 
   if (company?.documents?.companyLogoUrl) {
     try {
