8cae4445f1f70e32e659c4866e47c49cc9425885
rohit15rawat <rohitrawat0212@gmail.com>
2026-05-07 14:51:10 +0530

    changes

--- src/components/template/invoice/invoiceTemplatRolaface.tsx
+++ src/components/template/invoice/invoiceTemplatRolaface.tsx
@@ -109,11 +109,11 @@ doc.setFontSize(8);
   if (compEmail) { doc.text(compEmail, M, curY); curY += 4; }
 
   const logoRightX = MR;
-  const logoTopY = 21;
+  const logoTopY = 17;
 
   if (company?.documents?.companyLogoUrl) {
     try {
-      doc.addImage(px(company.documents.companyLogoUrl), "PNG", logoRightX - 70, logoTopY, 70, 55);
+      doc.addImage(px(company.documents.companyLogoUrl), "PNG", logoRightX - 70, logoTopY, 70, 45);
     } catch {}
   } else {
 
