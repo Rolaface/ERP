dbf0e48ab5a5c20265c67a0b43a33d910a846f5e
dixantnegi001 <DIXANT.NEGI@ROLAFACE.COM>
2026-05-20 10:25:13 +0530

    refactor: remove unused import of PurchaseOrderDetailModal in PurchaseOrdersTable component

--- src/views/Procurement/PurchaseOrders.tsx
+++ src/views/Procurement/PurchaseOrders.tsx
@@ -43,7 +43,6 @@ type OutletContextType = {
   openPOCreate: () => void;
   openPOEdit: (poId: string | number) => void;
 };
-import PurchaseOrderDetailModal from "../../components/procurement/purchaseorder/PurchaseOrderDetailsModal";
 
 
 
